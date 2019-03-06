fae320949a3d2df650ac8928bbab54e859cb56e2
arnav-t <avznav@gmail.com>
2019-03-06 20:31:17 +0530

    Add rooms list

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 require('./styles/styles.css');
 var sdk = require('matrix-js-sdk');
 
-console.log('Hello world!');
+var client = sdk.createClient("https://matrix.org");
+
+roomsList = document.getElementById('rooms-list');
+client.publicRooms(function(err, data) {
+	for(var room in data['chunk']) {
+		let name = data['chunk'][room]['canonical_alias'];
+		let listObj = document.createElement('li');
+		listObj.innerHTML = name;
+		roomsList.appendChild(listObj);
+	}
+});
