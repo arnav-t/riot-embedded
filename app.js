08e87f625ee8c3b1566930c06e9fff2a92509ab1
Ajay Ramachandran <dev@ajay.app>
2021-07-28 14:24:35 -0400

    Add support for display name, initial value and url parameters

--- app.js
+++ app.js
@@ -6,6 +6,33 @@ import ThemeContext from './components/theme-context.jsx';
 require('./styles/main.scss');
 
 ReactDOM.render(
-    <Client {...config} />,
+    <Client {...{...config, ...getConfigFromURL()}} />,
     document.getElementById('root')
 );
+
+function getConfigFromURL() {
+    if (!config.urlParameters) return {};
+
+    const hash = window.location.hash.substr(1);
+    const params = hash.split('&').reduce((acc, param) => {
+        const [key, value] = param.split('=');
+        acc[key] = parseValue(value);
+        console.log(value)
+        return acc;
+    }, {});
+
+    console.log(params)
+
+    return params;
+}
+
+function parseValue(value) {
+    if (value === 'true') {
+        return true;
+    } else if (value === 'false') {
+        return false;
+    } else if (Number.isFinite(value)) {
+        return Number(value);
+    }
+    return decodeURI(value);
+}
